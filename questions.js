855fde2d6bb6d7fe6f477fb12d86f39c11521e35
KevinKallo <90027927+KevinKallo@users.noreply.github.com>
2022-11-15 11:05:39 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,16 +1,33 @@
 import inquirer from 'inquirer';
+import Checkbox from 'prompt-checkbox';
 inquirer
     .prompt([
-        {type:'input',name:'age',message:'How old are you?'},  
-        {type:'input',name:'gender',message:'What is your gender?'},
-        {type:'input',name:'name',message:'What is your name?'},
-        {type:'input',name:'lastname',message:'What is your lastname?'},
-        {type:'input',name:'eyecolor',message:'What is your eyecolor?'},        
+        {type:'confirm',name:'confirm',message:'Are you real?'},  
+        {type:'password',name:'password',message:'Say something?', mask:''},
+        {type:'checkbox',name:'checkbox',message:'What is your sex?', choices: ['female','male','other',]},
+        {type:'rawlist',name:'rawlist',message:'Millise neist valiksid?', choices: ['Sinine','Punane','Kollane',]},
+        {type:'input',name:'eyecolor',message:'Kus su ema on?'},
+        {type:'list',name:'list',message:'What is your eyecolor?', choices: ["Sinine","roheline","pruun","hall",]},
+        {type:'editor',name:'editor',message:''}, 
+        {type:'expand',name:'expand',message:'gay või mitte?', choices: [{key:"y",value:"yes"},{key:"n",value:"no"}]},
+        {type:'number',name:'number',message:'mis on sinu IQ?'}, 
+
     ])
     .then((answer)=> {
-        console.log('I am ' + answer.age + ' years old');
-        console.log('I am ' + answer.gender);
-        console.log('My name is ' + answer.name);
-        console.log('My last name is ' + answer.lastname);
-        console.log('My eyecolor is ' + answer.eyecolor);
+        console.log(answer.confirm);
+        console.log(`sa ütlesid ${answer.password}`);
+        console.log(`You are ${answer.checkbox}`);
+        console.log(`You like ${answer.rawlist}`);
+        console.log(answer.eyecolor);
+        console.log(`Mu silma värv on ${answer.list}`);
+        console.log(` ${answer.editor}`);
+        console.log(` ${answer.expand}`);
+        console.log(` ${answer.number}`);
+        
+
+
+
     })
+    
+
+    
